82567306a48d056d305ff4fbeb607b1e865835eb
WMacalester <willjpmaca@hotmail.com>
2023-01-14 15:21:44 +0000

    Style: prettier

--- planner-ui/src/api/api.ts
+++ planner-ui/src/api/api.ts
@@ -1,12 +1,11 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import { BASE_URL } from '../constants';
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { BASE_URL } from "../constants";
 
 export const apiSlice = createApi({
-    reducerPath: 'api',
-    baseQuery: fetchBaseQuery({
-        baseUrl: `${BASE_URL}`,
-    }),
-    tagTypes: [
-    ],
-    endpoints: () => ({}),
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({
+    baseUrl: `${BASE_URL}`,
+  }),
+  tagTypes: [],
+  endpoints: () => ({}),
 });
--- planner-ui/src/store.ts
+++ planner-ui/src/store.ts
@@ -1,13 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "./api/api";
 
-
 const store = configureStore({
-    reducer: {
-        [apiSlice.reducerPath]: apiSlice.reducer,
-    },
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware({serializableCheck: false}).concat(apiSlice.middleware),
-})
+  reducer: {
+    [apiSlice.reducerPath]: apiSlice.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({ serializableCheck: false }).concat(
+      apiSlice.middleware
+    ),
+});
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
