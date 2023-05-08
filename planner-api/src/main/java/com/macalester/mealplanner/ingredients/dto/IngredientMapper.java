b351ec62b9afac1298bcdb83bf03b0218fe2d89e
WMacalester <willjpmaca@hotmail.com>
2023-05-08 11:08:02 +0100

    Feat: Map UUID to ingredient

--- planner-api/src/main/java/com/macalester/mealplanner/ingredients/dto/IngredientMapper.java
+++ planner-api/src/main/java/com/macalester/mealplanner/ingredients/dto/IngredientMapper.java
@@ -3,15 +3,13 @@ package com.macalester.mealplanner.ingredients.dto;
 import com.macalester.mealplanner.ingredients.Ingredient;
 import java.util.List;
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface IngredientMapper {
-  IngredientMapper INSTANCE = Mappers.getMapper(IngredientMapper.class);
-
   IngredientDto ingredientToDto(Ingredient ingredient);
 
   List<IngredientDto> ingredientToDto(List<Ingredient> ingredients);
 
   Ingredient ingredientCreateDTOtoIngredient(IngredientCreateDto ingredientCreateDTO);
+
 }
