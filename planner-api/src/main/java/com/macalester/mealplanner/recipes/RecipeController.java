bc5c332982927b79e52fe47ce2eb2e55fc2cdd9c
WMacalester <willjpmaca@hotmail.com>
2023-05-08 13:57:09 +0100

    Feat: Add Recipe API

--- planner-api/src/main/java/com/macalester/mealplanner/recipes/RecipeController.java
+++ planner-api/src/main/java/com/macalester/mealplanner/recipes/RecipeController.java
@@ -1,24 +1,44 @@
 package com.macalester.mealplanner.recipes;
 
+import com.macalester.mealplanner.exceptions.NotFoundException;
+import com.macalester.mealplanner.exceptions.UniqueConstraintViolationException;
+import com.macalester.mealplanner.recipes.dto.RecipeCreateDto;
+import com.macalester.mealplanner.recipes.dto.RecipeCreateDtoMapper;
+import com.macalester.mealplanner.recipes.dto.RecipeDto;
+import com.macalester.mealplanner.recipes.dto.RecipeMapper;
+import jakarta.validation.Valid;
 import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+import org.mapstruct.factory.Mappers;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/recipes")
 public class RecipeController {
 
   private final RecipeService recipeService;
-
-  @Autowired
-  public RecipeController(RecipeService recipeService) {
-    this.recipeService = recipeService;
-  }
+  private final RecipeMapper recipeMapper = Mappers.getMapper(RecipeMapper.class);
+  private final RecipeCreateDtoMapper recipeCreateDtoMapper;
 
   @GetMapping
   public List<Recipe> getAllRecipes() {
     return recipeService.getAllRecipes();
+  }
+
+  @PostMapping
+  public RecipeDto addRecipe(@Valid @RequestBody RecipeCreateDto recipeCreateDto) {
+    try {
+      Recipe newRecipe = recipeCreateDtoMapper.apply(recipeCreateDto);
+      return recipeMapper.recipeToDto(recipeService.addRecipe(newRecipe));
+    } catch (UniqueConstraintViolationException | NotFoundException e) {
+      throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+    }
   }
 }
