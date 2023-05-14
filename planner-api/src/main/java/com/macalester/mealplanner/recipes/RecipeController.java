c6c51c5d8e3066c86fc730fe0d4cb23b883fb508
WMacalester <willjpmaca@hotmail.com>
2023-05-14 11:22:04 +0100

    Feat: Delete recipe by id - controller
    Test

--- planner-api/src/main/java/com/macalester/mealplanner/recipes/RecipeController.java
+++ planner-api/src/main/java/com/macalester/mealplanner/recipes/RecipeController.java
@@ -11,11 +11,13 @@ import java.util.List;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -50,5 +52,11 @@ public class RecipeController {
     } catch (UniqueConstraintViolationException | NotFoundException e) {
       throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
     }
+  }
+
+  @DeleteMapping("/{id}")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  public void deleteRecipeById(@PathVariable UUID id) {
+    recipeService.deleteById(id);
   }
 }
