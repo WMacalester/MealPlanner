3e1efb74fe01e980b8c8fe8ad6d923e3595ed51e
WMacalester <willjpmaca@hotmail.com>
2023-05-21 17:25:27 +0100

    Feat: Recipe entity setters

--- planner-api/src/main/java/com/macalester/mealplanner/recipes/Recipe.java
+++ planner-api/src/main/java/com/macalester/mealplanner/recipes/Recipe.java
@@ -14,11 +14,13 @@ import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
+@Setter
 public class Recipe {
   @Id
   @GeneratedValue(strategy = GenerationType.UUID)
