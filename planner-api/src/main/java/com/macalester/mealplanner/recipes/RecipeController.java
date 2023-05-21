663e5a0013f5dedff09555648d4c0561cd3832bc
WMacalester <willjpmaca@hotmail.com>
2023-05-21 17:31:09 +0100

    Feat: Edit existing recipe by id - controller
    Tests

--- planner-api/src/main/java/com/macalester/mealplanner/recipes/RecipeController.java
+++ planner-api/src/main/java/com/macalester/mealplanner/recipes/RecipeController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -58,5 +59,21 @@ public class RecipeController {
   @ResponseStatus(HttpStatus.NO_CONTENT)
   public void deleteRecipeById(@PathVariable UUID id) {
     recipeService.deleteById(id);
+  }
+
+  @PutMapping("/{id}")
+  public RecipeDto editRecipeById(
+      @PathVariable UUID id, @RequestBody @Valid RecipeCreateDto recipeCreateDto) {
+    try {
+      return recipeDtoMapper.apply(
+          recipeService.editRecipeById(id, recipeCreateDtoMapper.apply(recipeCreateDto)));
+    } catch (UniqueConstraintViolationException e) {
+      throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+    } catch (NotFoundException e) {
+      if (e.getMessage().toLowerCase().contains("recipe")) {
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
+      }
+      throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+    }
   }
 }
