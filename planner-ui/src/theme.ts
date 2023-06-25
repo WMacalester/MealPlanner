58b58832bdf1c909ec55a6634870dc85ca13f07b
WMacalester <willjpmaca@hotmail.com>
2023-06-25 16:04:19 +0100

    Config: Update UI theme

--- planner-ui/src/theme.ts
+++ planner-ui/src/theme.ts
@@ -1,17 +1,47 @@
 import { createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
-  palette: {
-    contrastThreshold: 4.5,
-    primary: {
-      light: "#0066aa",
-      main: "#0044aa",
+    palette: {
+        contrastThreshold: 4.5,
+        primary: {
+            light: "#0e79b2",
+            main: "#0E79B2",
+        },
+        secondary: {
+            main: "#BF1363",
+            contrastText: "#ffcc00",
+        },
+        tertiary: {
+            main: "#F39237"
+        },
+        highlights: {
+            main: "#FBFEF9",
+            contrastText: "#fff",
+        },
+        base:{
+            main: "#191923",
+        },
     },
-    secondary: {
-      main: "#aa0099",
-      contrastText: "#ffcc00",
+
+    typography: {
+        button: {
+            fontSize: '1.3rem',
+        }
     },
-  },
 });
 
 export default theme;
+
+declare module "@mui/material/styles" {
+    interface Palette {
+        highlights: PaletteOptions["primary"];
+        base: PaletteOptions["primary"];
+        tertiary: PaletteOptions["primary"];
+    }
+
+    interface PaletteOptions {
+        highlights?: PaletteOptions["primary"];
+        base?: PaletteOptions["primary"];
+        tertiary?: PaletteOptions["primary"];
+    }
+}
