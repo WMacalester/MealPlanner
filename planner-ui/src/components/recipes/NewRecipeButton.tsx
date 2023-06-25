466d6a166c77ddb90060d0fcbbc89d39e13abbc5
WMacalester <willjpmaca@hotmail.com>
2023-06-25 15:48:07 +0100

    Feat: Add Recipe Modal and Button

---
+++ planner-ui/src/components/recipes/NewRecipeButton.tsx
@@ -0,0 +1,26 @@
+import { IconButton } from "@mui/material";
+import { useState, FC } from "react";
+import NewRecipeModal from "./NewRecipeModal";
+import PostAddIcon from "@mui/icons-material/PostAdd";
+
+const NewRecipeButton: FC = () => {
+    const [openNewRecipeModal, setOpenNewRecipeModal] = useState(false);
+    const handleOpen = () => setOpenNewRecipeModal(true);
+    const handleClose = () => {
+        setOpenNewRecipeModal(false);
+    };
+
+    return (
+        <div>
+            <IconButton aria-label="Add new recipe" onClick={handleOpen}>
+                <PostAddIcon />
+            </IconButton>
+            <NewRecipeModal
+                open={openNewRecipeModal}
+                handleClose={handleClose}
+            />
+        </div>
+    );
+};
+
+export default NewRecipeButton;
