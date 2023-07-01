e628b6f0d8fdf64a68ac8ac57a2d09ba52d9c93d
WMacalester <willjpmaca@hotmail.com>
2023-07-01 15:12:38 +0100

    Feat: Update page styling, add navbar

--- planner-ui/src/App.tsx
+++ planner-ui/src/App.tsx
@@ -1,14 +1,23 @@
-import Recipes from "./components/recipes/RecipeBoard";
+import RecipeBoard from "./components/recipes/RecipeBoard";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "./theme";
+import { Box } from "@mui/material";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <div className="App">
-        <header className="App-header"></header>
-        <Recipes />
-      </div>
+      <Box height={"100%"} bgcolor={"base.main"}>
+        <Navbar />
+        <Box
+          height={"90%"}
+          alignItems={"center"}
+          justifyContent={"center"}
+          display={"flex"}
+        >
+          <RecipeBoard />
+        </Box>
+      </Box>
     </ThemeProvider>
   );
 }
