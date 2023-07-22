1c29ccca16d9d7c4b1d4c57d1fc3cbc265eacd1b
WMacalester <willjpmaca@hotmail.com>
2023-07-22 15:42:21 +0100

    Feat: ConstraintViolationControllerAdvice

---
+++ planner-api/src/main/java/com/macalester/mealplanner/ConstraintViolationControllerAdvice.java
@@ -0,0 +1,59 @@
+package com.macalester.mealplanner;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class ConstraintViolationControllerAdvice {
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    ValidationErrorResponse onConstraintValidationException(
+            ConstraintViolationException e) {
+        ValidationErrorResponse error = new ValidationErrorResponse();
+
+        for (ConstraintViolation<?> violation : e.getConstraintViolations()) {
+            error.getViolations().add(
+                    new Violation(violation.getPropertyPath().toString(), violation.getMessage()));
+        }
+        return error;
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    ValidationErrorResponse onMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        ValidationErrorResponse error = new ValidationErrorResponse();
+
+        for (FieldError fieldError : e.getBindingResult().getFieldErrors()) {
+            error.getViolations().add(
+                    new Violation(fieldError.getField(), fieldError.getDefaultMessage()));
+        }
+        return error;
+    }
+
+    @Getter
+    private static class ValidationErrorResponse {
+        private final List<Violation> violations = new ArrayList<>();
+    }
+
+    @Getter
+    @RequiredArgsConstructor
+    private static class Violation {
+        private final String field;
+        private final String message;
+    }
+
+}
