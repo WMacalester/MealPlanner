5743b7583f5bea5a4e40ffb7695cc9d2ad12bd86
WMacalester <willjpmaca@hotmail.com>
2023-07-23 14:38:32 +0100

    Refactor: App to include MenuBoard

--- planner-ui/src/App.tsx
+++ planner-ui/src/App.tsx
@@ -1,22 +1,42 @@
 import RecipeBoard from "./components/recipes/RecipeBoard";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "./theme";
-import { Box } from "@mui/material";
+import { Box, Grid } from "@mui/material";
 import Navbar from "./components/navbar/Navbar";
+import Menuboard from "./components/menu/Menuboard";
+import { NAVBAR_HEIGHT } from "./constants";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
+      <Navbar />
       <Box height={"100%"} bgcolor={"base.main"}>
-        <Navbar />
-        <Box
-          height={"90%"}
-          alignItems={"center"}
-          justifyContent={"center"}
-          display={"flex"}
+        <Grid
+          container
+          direction={"row"}
+          height={"100%"}
+          justifyContent={"space-between"}
+          paddingX={"5%"}
+          paddingTop={NAVBAR_HEIGHT}
         >
-          <RecipeBoard />
-        </Box>
+          <Grid
+            item
+            xs
+            alignItems={"center"}
+            justifyContent={"center"}
+            display="flex"
+          >
+            <RecipeBoard />
+          </Grid>
+          <Grid
+            item
+            alignItems={"center"}
+            justifyContent={"center"}
+            display="flex"
+          >
+            <Menuboard />
+          </Grid>
+        </Grid>
       </Box>
     </ThemeProvider>
   );
