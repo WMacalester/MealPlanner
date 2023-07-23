a962e366e5b7bc4e07f454b2e0190a22aac2f87a
WMacalester <willjpmaca@hotmail.com>
2023-07-23 11:11:44 +0100

    Feat: FindIngredientByName

--- planner-api/src/main/java/com/macalester/mealplanner/ingredients/IngredientRepository.java
+++ planner-api/src/main/java/com/macalester/mealplanner/ingredients/IngredientRepository.java
@@ -1,5 +1,6 @@
 package com.macalester.mealplanner.ingredients;
 
+import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -7,4 +8,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface IngredientRepository extends JpaRepository<Ingredient, UUID> {
   boolean existsByName(String name);
+  Optional<Ingredient> findByName(String name);
 }
