e1c4eea92502bc7a52176a6e8e804ca47180a220
WMacalester <willjpmaca@hotmail.com>
2023-07-23 13:22:00 +0100

    Feat: RerollButton

---
+++ planner-ui/src/components/button/RerollButton.tsx
@@ -0,0 +1,34 @@
+import { Button } from "@mui/material";
+import { FC } from "react";
+import CasinoIcon from "@mui/icons-material/Casino";
+
+interface RerollButtonProps {
+  handleClick: () => void;
+}
+
+const RerollButton: FC<RerollButtonProps> = ({ handleClick }) => {
+  return (
+    <div>
+      <Button
+        aria-label={`Generate menu button`}
+        onClick={handleClick}
+        endIcon={<CasinoIcon />}
+        variant="outlined"
+        sx={{
+          borderColor: "black",
+          color: "black",
+          marginX: "1rem",
+          ":hover": {
+            color: "highlights.main",
+            backgroundColor: "red",
+            borderColor: "red",
+          },
+        }}
+      >
+        Menu
+      </Button>
+    </div>
+  );
+};
+
+export default RerollButton;
