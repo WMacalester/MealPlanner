f26c2ef11f873b0a39ddcb4fde33c923ba76eea1
WMacalester <willjpmaca@hotmail.com>
2023-08-01 17:55:16 +0100

    Feat: Find all recipes by ids

--- planner-api/src/main/java/com/macalester/mealplanner/recipes/RecipeService.java
+++ planner-api/src/main/java/com/macalester/mealplanner/recipes/RecipeService.java
@@ -13,54 +13,60 @@ import org.springframework.stereotype.Service;
 @Service
 public class RecipeService {
 
-  private final RecipeRepository recipeRepository;
+    private final RecipeRepository recipeRepository;
 
-  @Autowired
-  public RecipeService(RecipeRepository recipeRepository) {
-    this.recipeRepository = recipeRepository;
-  }
-
-  public List<Recipe> getAllRecipes() {
-    return recipeRepository.findAll();
-  }
-
-  public Recipe findById(UUID id) {
-    return recipeRepository
-        .findById(id)
-        .orElseThrow(
-            () -> new NotFoundException(String.format("Recipe with id %s was not found", id)));
-  }
-
-  public Recipe addRecipe(Recipe recipe) {
-    if (recipeRepository.existsByName(recipe.getName())) {
-      throw new UniqueConstraintViolationException(
-          String.format("Recipe with name %s already exists", recipe.getName()));
-    }
-    return recipeRepository.save(recipe);
-  }
-
-  public void deleteById(UUID id) {
-    recipeRepository.deleteById(id);
-  }
-
-  public Recipe editRecipeById(UUID id, Recipe updatedRecipe) {
-    Recipe storedRecipe = findById(id);
-
-    if (!storedRecipe.getName().equals(updatedRecipe.getName())
-        && recipeRepository.existsByName(updatedRecipe.getName())) {
-      throw new UniqueConstraintViolationException(
-          String.format("Recipe with name %s already exists", updatedRecipe.getName()));
+    @Autowired
+    public RecipeService(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
     }
 
-    updatedRecipe.setId(storedRecipe.getId());
-    return recipeRepository.save(updatedRecipe);
-  }
+    public List<Recipe> getAllRecipes() {
+        return recipeRepository.findAll();
+    }
 
-  public Set<Recipe> getRecipesNotInCollection(Collection<Recipe> recipeCollection){
-      if (recipeCollection == null || recipeCollection.isEmpty()){
-          return new HashSet<>(recipeRepository.findAll());
-      }
+    public Recipe findById(UUID id) {
+        return recipeRepository
+                .findById(id)
+                .orElseThrow(
+                        () -> new NotFoundException(String.format("Recipe with id %s was not found", id)));
+    }
 
-      return recipeRepository.findAllNotInCollection(recipeCollection);
-  }
+    public Set<Recipe> findAllById(Set<UUID> ids) {
+        Set<Recipe> output = new HashSet<>(ids.size());
+        ids.forEach(id -> output.add(findById(id)));
+        return output;
+    }
+
+    public Recipe addRecipe(Recipe recipe) {
+        if (recipeRepository.existsByName(recipe.getName())) {
+            throw new UniqueConstraintViolationException(
+                    String.format("Recipe with name %s already exists", recipe.getName()));
+        }
+        return recipeRepository.save(recipe);
+    }
+
+    public void deleteById(UUID id) {
+        recipeRepository.deleteById(id);
+    }
+
+    public Recipe editRecipeById(UUID id, Recipe updatedRecipe) {
+        Recipe storedRecipe = findById(id);
+
+        if (!storedRecipe.getName().equals(updatedRecipe.getName())
+                && recipeRepository.existsByName(updatedRecipe.getName())) {
+            throw new UniqueConstraintViolationException(
+                    String.format("Recipe with name %s already exists", updatedRecipe.getName()));
+        }
+
+        updatedRecipe.setId(storedRecipe.getId());
+        return recipeRepository.save(updatedRecipe);
+    }
+
+    public Set<Recipe> getRecipesNotInCollection(Collection<Recipe> recipeCollection) {
+        if (recipeCollection == null || recipeCollection.isEmpty()) {
+            return new HashSet<>(recipeRepository.findAll());
+        }
+
+        return recipeRepository.findAllNotInCollection(recipeCollection);
+    }
 }
