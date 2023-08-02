96d7dbb1aaa7d8e45ac1383d89fc8baee6d9d23c
WMacalester <willjpmaca@hotmail.com>
2023-08-02 12:24:47 +0100

    Feat: SelectRecipeButton

---
+++ planner-ui/src/components/button/SelectRecipeButton.tsx
@@ -0,0 +1,32 @@
+import { FC } from "react";
+import { IconButton } from "@mui/material";
+import PlaylistRemoveIcon from "@mui/icons-material/PlaylistRemove";
+import PlaylistAddIcon from "@mui/icons-material/PlaylistAdd";
+
+interface SelectRecipeButtonProps {
+  isSelected: boolean;
+  name: string;
+  handleClick: () => void;
+}
+
+const SelectRecipeButton: FC<SelectRecipeButtonProps> = ({
+  isSelected,
+  name,
+  handleClick,
+}) => {
+  return (
+    <div>
+      <IconButton
+        aria-label={`Select ${name} button`}
+        onClick={handleClick}
+        sx={{
+          color: isSelected ? "highlights.main" : "highlights.main",
+        }}
+      >
+        {isSelected ? <PlaylistRemoveIcon /> : <PlaylistAddIcon />}
+      </IconButton>
+    </div>
+  );
+};
+
+export default SelectRecipeButton;
