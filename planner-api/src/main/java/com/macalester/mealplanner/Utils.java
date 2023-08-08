927e4761fb707fcf77e1226cff9ad8b6c5daaade
WMacalester <willjpmaca@hotmail.com>
2023-08-08 21:25:34 +0100

    Feat: Format filename for export

--- planner-api/src/main/java/com/macalester/mealplanner/Utils.java
+++ planner-api/src/main/java/com/macalester/mealplanner/Utils.java
@@ -1,9 +1,17 @@
 package com.macalester.mealplanner;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Utils {
     private Utils(){}
 
     public static String formatName(String name){
         return name.trim().toLowerCase().replaceAll(" +", " ");
+    }
+
+    public static String computeFilename(String filePrefix) {
+        SimpleDateFormat date = new SimpleDateFormat("yyyyMMdd");
+        return filePrefix + "_" + date.format(new Date()) + ".csv";
     }
 }
