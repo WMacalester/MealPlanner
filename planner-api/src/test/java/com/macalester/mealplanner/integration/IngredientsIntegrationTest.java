fce357610685a02f97a572bb58bd25aa6802923b
WMacalester <willjpmaca@hotmail.com>
2023-08-19 17:44:27 +0100

    Feat: Add authorization requirements to rest of IngredientController endpoints
    Migrate IngredientController unit tests to integration tests
    
    GetIngredientById
    DeleteIngredientById
    EditIngredientById

--- planner-api/src/test/java/com/macalester/mealplanner/integration/IngredientsIntegrationTest.java
+++ planner-api/src/test/java/com/macalester/mealplanner/integration/IngredientsIntegrationTest.java
@@ -3,15 +3,17 @@ package com.macalester.mealplanner.integration;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.core.type.TypeReference;
-import com.macalester.mealplanner.AuthOpenSecurityConfig;
 import com.macalester.mealplanner.AuthenticationActiveInitializer;
 import com.macalester.mealplanner.ingredients.Ingredient;
 import com.macalester.mealplanner.ingredients.IngredientRepository;
 import com.macalester.mealplanner.ingredients.dto.IngredientCreateDto;
+import com.macalester.mealplanner.ingredients.dto.IngredientDtoMapper;
+import com.macalester.mealplanner.ingredients.dto.IngredientEditDto;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
@@ -26,11 +28,12 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-//@ContextConfiguration(classes = {AuthOpenSecurityConfig.class})
 @ContextConfiguration(initializers = AuthenticationActiveInitializer.class)
 public class IngredientsIntegrationTest extends BaseIntegrationTest {
     @Autowired
     private IngredientRepository ingredientRepository;
+
+    @Autowired private IngredientDtoMapper ingredientDtoMapper;
 
     private static final String name1 = "ingredient a";
     private static final String name2 = "ingredient b";
@@ -115,7 +118,7 @@ public class IngredientsIntegrationTest extends BaseIntegrationTest {
         class CsvTest {
             @Test
             @DisplayName("Get all ingredients returns all ingredients in database in csv format")
-            @WithMockUser(roles="ADMIN")
+            @WithMockUser(roles = "ADMIN")
             void getAllIngredients_givenIngredientsInDb_returnsAllIngredientsInCSV() throws Exception {
                 ingredientRepository.save(ingredient1);
                 ingredientRepository.save(ingredient2);
@@ -129,15 +132,15 @@ public class IngredientsIntegrationTest extends BaseIntegrationTest {
                                 .getResponse()
                                 .getContentAsString();
 
-                String expected = String.join("\n", name1,name2,name3);
+                String expected = String.join("\n", name1, name2, name3);
 
                 assertEquals(expected, responseBody);
             }
 
             @Test
             @DisplayName("User has role User, returns 403")
-            @WithMockUser(roles="USER")
-            void getAllIngredients_userHasRoleUser_returns403V() throws Exception {
+            @WithMockUser(roles = "USER")
+            void getAllIngredients_userHasRoleUser_returns403() throws Exception {
                 mockMvc
                         .perform(MockMvcRequestBuilders.get("/ingredients").accept("text/csv"))
                         .andExpect(status().isForbidden());
@@ -202,5 +205,139 @@ public class IngredientsIntegrationTest extends BaseIntegrationTest {
                     .andExpect(status().isForbidden());
         }
 
+    }
+
+    @Nested
+    @DisplayName("Get ingredient by Id")
+    class GetIngredientByIdTest {
+        @Test
+        @DisplayName("Returns ingredientDto with id in db")
+        @WithMockUser(roles = "USER")
+        void getIngredientById_givenIngredientWithIdInDb_returnsIngredient() throws Exception {
+            Ingredient savedIngredient = ingredientRepository.save(ingredient1);
+            String expected = objectMapper.writeValueAsString(ingredientDtoMapper.apply(savedIngredient));
+
+            mockMvc
+                    .perform(MockMvcRequestBuilders.get("/ingredients/" + savedIngredient.getId()))
+                    .andExpect(status().isOk())
+                    .andExpect(
+                            MockMvcResultMatchers.content()
+                                    .string(equalTo(expected)));
+        }
+
+        @Test
+        @DisplayName("Returns 404 for ingredient with id not in db")
+        @WithMockUser(roles = "USER")
+        void getIngredientById_givenIngredientWithIdNotInDb_returns404() throws Exception {
+            mockMvc
+                    .perform(MockMvcRequestBuilders.get("/ingredients/" + UUID.randomUUID()))
+                    .andExpect(status().isNotFound());
+        }
+    }
+
+
+    @Nested
+    @DisplayName("Edit ingredient by id")
+    class EditIngredientByIdTest {
+
+        private static final String newIngredientName = "new ingredient name";
+        private final IngredientEditDto ingredientEditDto = new IngredientEditDto(newIngredientName);
+        private final IngredientEditDto ingredientEditDto_blankName =
+                new IngredientEditDto("        \n  ");
+
+        @Test
+        @DisplayName("Name in request is unique, returns ingredient with updated name")
+        @WithMockUser(roles = "ADMIN")
+        void editIngredientById_givenUniqueName_returnsIngredientWithUpdatedName() throws Exception {
+            Ingredient ingredientSaved = ingredientRepository.save(ingredient1);
+            ingredientSaved.setName(newIngredientName);
+            String expected = objectMapper.writeValueAsString(ingredientDtoMapper.apply(ingredientSaved));
+
+            mockMvc
+                    .perform(
+                            MockMvcRequestBuilders.patch("/ingredients/" + ingredientSaved.getId())
+                                    .contentType(MediaType.APPLICATION_JSON)
+                                    .content(objectMapper.writeValueAsString(ingredientEditDto)))
+                    .andExpect(status().isOk())
+                    .andExpect(content().string(equalTo(expected)));
+        }
+
+        @Test
+        @DisplayName("Name in request is not unique, returns 400")
+        @WithMockUser(roles = "ADMIN")
+        void editIngredientById_givenNameAndIsNotUnique_returns400() throws Exception {
+            Ingredient ingredientSaved = ingredientRepository.save(ingredient1);
+            IngredientEditDto ingredientEditDto_nonUniqueName = new IngredientEditDto(ingredientSaved.getName());
+
+            mockMvc
+                    .perform(
+                            MockMvcRequestBuilders.patch("/ingredients/" + ingredientSaved.getId())
+                                    .contentType(MediaType.APPLICATION_JSON)
+                                    .content(objectMapper.writeValueAsString(ingredientEditDto_nonUniqueName)))
+                    .andExpect(status().isBadRequest());
+        }
+
+        @Test
+        @DisplayName("Request ingredient id not in db returns 404")
+        @WithMockUser(roles = "ADMIN")
+        void editIngredientById_givenIngredientWithIdNotInDb_returns404() throws Exception {
+            mockMvc
+                    .perform(
+                            MockMvcRequestBuilders.patch("/ingredients/" + UUID.randomUUID())
+                                    .contentType(MediaType.APPLICATION_JSON)
+                                    .content(objectMapper.writeValueAsString(ingredientEditDto)))
+                    .andExpect(status().isNotFound());
+        }
+
+        @Test
+        @DisplayName("Name in request is blank, returns 400")
+        @WithMockUser(roles = "ADMIN")
+        void editIngredientById_givenBlankName_returns400() throws Exception {
+            mockMvc
+                    .perform(
+                            MockMvcRequestBuilders.patch("/ingredients/" + UUID.randomUUID())
+                                    .contentType(MediaType.APPLICATION_JSON)
+                                    .content(objectMapper.writeValueAsString(ingredientEditDto_blankName)))
+                    .andExpect(status().isBadRequest());
+        }
+
+        @Test
+        @DisplayName("User has role user, returns 403")
+        @WithMockUser(roles = "USER")
+        void editIngredientById_userHasRoleUser_returns403() throws Exception {
+            mockMvc
+                    .perform(
+                            MockMvcRequestBuilders.patch("/ingredients/" + UUID.randomUUID())
+                                    .contentType(MediaType.APPLICATION_JSON)
+                                    .content(objectMapper.writeValueAsString(ingredientEditDto)))
+                    .andExpect(status().isForbidden());
+        }
+
+    }
+
+    @Nested
+    @DisplayName("Delete ingredient by Id")
+    class DeleteIngredientByIdTest {
+        @Test
+        @DisplayName("Delete ingredient by id")
+        @WithMockUser(roles = "ADMIN")
+        void deleteIngredientById_givenUUIDAndIngredientInDb_returns204() throws Exception {
+            ingredientRepository.save(ingredient1);
+
+            mockMvc
+                    .perform(MockMvcRequestBuilders.delete("/ingredients/" + ingredient1.getId()))
+                    .andExpect(status().isNoContent());
+        }
+
+        @Test
+        @DisplayName("User has role User, returns 403")
+        @WithMockUser(roles = "USER")
+        void deleteIngredientById_givenUserHasRoleUser_returns403() throws Exception {
+            ingredientRepository.save(ingredient1);
+
+            mockMvc
+                    .perform(MockMvcRequestBuilders.delete("/ingredients/" + ingredient1.getId()))
+                    .andExpect(status().isForbidden());
+        }
     }
 }
