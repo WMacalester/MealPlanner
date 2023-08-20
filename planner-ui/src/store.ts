0fd023accdf2e10fb0c3435a31a851f4ff9dfffd
WMacalester <willjpmaca@hotmail.com>
2023-08-20 22:16:33 +0100

    Feat: add auth login and token refresh to ui
    BaseQueryWithReauth to handle token refreshing in scenarios where accessToken ha expired
    Add mutations for registering and logging in
    Add authSlice for handling state of signed in user

--- planner-ui/src/store.ts
+++ planner-ui/src/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "./api/api";
 import selectedRecipesReducer from "./selectedRecipesReducer";
+import authReducer from "./api/authSlice";
 
 const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
+    auth: authReducer,
     selectedRecipeIds: selectedRecipesReducer,
   },
   middleware: (getDefaultMiddleware) =>
