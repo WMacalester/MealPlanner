3882faf4b31a0cadd08303e6f65c68f9842cba67
WMacalester <willjpmaca@hotmail.com>
2023-08-31 22:15:39 +0100

    Feat: login and register form

---
+++ planner-ui/src/components/button/LoginDialogButton.tsx
@@ -0,0 +1,31 @@
+import { Button } from "@mui/material";
+import { FC } from "react";
+
+interface LoginDialogButtonProps {
+  handleClick: (e: React.MouseEvent) => void;
+  name: string;
+}
+
+const LoginDialogButton: FC<LoginDialogButtonProps> = ({
+  handleClick,
+  name,
+}) => {
+  return (
+    <Button
+      onClick={handleClick}
+      sx={{
+        backgroundColor: "primary.main",
+        color: "highlights.main",
+        ":hover": {
+          color: "highlights.main",
+          backgroundColor: "secondary.main",
+          borderColor: "secondary.main",
+        },
+      }}
+    >
+      {name}
+    </Button>
+  );
+};
+
+export default LoginDialogButton;
