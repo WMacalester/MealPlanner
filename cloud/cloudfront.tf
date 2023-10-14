4d42771f911a2a063cd99c6f6a5c28bfbebc7032
WMacalester <willjpmaca@hotmail.com>
2023-10-14 15:02:37 +0100

    cloud: update cloudfront routing
    add domain alias
    add ui s3 origin
    add alb origin
    add routing back to index.html for react-router via error response
    add optional cloudfront logging config

--- cloud/cloudfront.tf
+++ cloud/cloudfront.tf
@@ -10,25 +10,46 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
     origin_id = aws_s3_bucket.ui_bucket.arn
   }
 
+  origin {
+    domain_name = aws_lb.load_balancer.dns_name
+    origin_id = aws_lb.load_balancer.id
+
+    custom_origin_config {
+      http_port = 80
+      https_port = 443
+      origin_protocol_policy = "https-only" 
+      origin_ssl_protocols = ["TLSv1.2"]
+    }
+  }
+
   enabled = true
   is_ipv6_enabled = true
   default_root_object = "index.html"
 
+  aliases = [var.domain_alias]
+
+  # Cache behavior for s3/ui
   default_cache_behavior {
     allowed_methods  = ["GET", "HEAD"]
     cached_methods   = ["GET", "HEAD"]
     target_origin_id = aws_s3_bucket.ui_bucket.arn
 
-    forwarded_values {
-      headers = []
-      query_string = false
-
-      cookies {
-        forward = "all"
-      }
-    }
+    cache_policy_id = aws_cloudfront_cache_policy.caching_optimized_policy.id
 
     viewer_protocol_policy = "redirect-to-https"
+  }
+
+  # Cache behavior with precedence 0 for loadbalancer/api
+  ordered_cache_behavior {
+    path_pattern     = "/api/*"
+    allowed_methods  = ["GET", "POST", "HEAD", "PUT", "PATCH", "DELETE", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD", "OPTIONS"]
+    target_origin_id = aws_lb.load_balancer.arn
+    
+    compress               = true
+    viewer_protocol_policy = "redirect-to-https"
+    cache_policy_id = aws_cloudfront_cache_policy.caching_disabled_policy.id
+    origin_request_policy_id = aws_cloudfront_origin_request_policy.all_viewer_policy.id
   }
 
   price_class = "PriceClass_100"
@@ -44,4 +65,88 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
     acm_certificate_arn = var.cloudfront_certificate_arn
     ssl_support_method = "sni-only"
   }
+
+  # logging_config {
+  #   bucket = aws_s3_bucket.cloudfront_logs.bucket_domain_name
+  #   include_cookies = false
+  #   prefix = "cloudfront-logs/"
+  # }
+
+  # For correct behaviour with react-router
+  custom_error_response {
+    error_code = 403
+    response_code = 200
+    response_page_path = "/index.html"
+  }
 }
+
+resource "aws_cloudfront_origin_request_policy" "all_viewer_policy" {
+  name    = "allViewer-policy"
+  comment = "allViewer comment"
+  cookies_config {
+    cookie_behavior = "all"
+  }
+  headers_config {
+    header_behavior = "allViewer"
+  }
+  query_strings_config {
+    query_string_behavior = "all"
+  }
+}
+
+resource "aws_cloudfront_cache_policy" "caching_optimized_policy" {
+  name        = "caching-optimized-policy"
+  default_ttl = 86400
+  max_ttl     = 31536000
+  min_ttl     = 1
+  parameters_in_cache_key_and_forwarded_to_origin {
+    cookies_config {
+      cookie_behavior = "none"
+    }
+    headers_config {
+      header_behavior = "none"
+    }
+    query_strings_config {
+      query_string_behavior = "none"
+    }
+  }
+}
+
+resource "aws_cloudfront_cache_policy" "caching_disabled_policy" {
+  name        = "caching-disabled-policy"
+  default_ttl = 0
+  max_ttl     = 0
+  min_ttl     = 0
+  parameters_in_cache_key_and_forwarded_to_origin {
+    cookies_config {
+      cookie_behavior = "none"
+    }
+    headers_config {
+      header_behavior = "none"
+    }
+    query_strings_config {
+      query_string_behavior = "none"
+    }
+  }
+}
+
+# ------
+# For logging:
+
+# resource "aws_s3_bucket" "cloudfront_logs" {
+#   bucket = "wmacalester-mealplanner-cloudfront-logs"
+# }
+
+# resource "aws_s3_bucket_ownership_controls" "cloudfront_logs_ownership_controls" {
+#   bucket = aws_s3_bucket.cloudfront_logs.id
+#   rule {
+#     object_ownership = "BucketOwnerPreferred"
+#   }
+# }
+
+# resource "aws_s3_bucket_acl" "ui_bucket_acl" {
+#   depends_on = [aws_s3_bucket_ownership_controls.cloudfront_logs_ownership_controls]
+
+#   bucket = aws_s3_bucket.cloudfront_logs.id
+#   acl    = "log-delivery-write"
+# }
--- cloud/variables.tf
+++ cloud/variables.tf
@@ -1,3 +1,7 @@
 variable "aws_region" {
   default = "eu-west-2"
 }
+
+variable "domain_alias" {
+  default = "mealplanner.wmacalester.co.uk"
+}
