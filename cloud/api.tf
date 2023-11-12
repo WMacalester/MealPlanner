4fbbc6d9b64665695b98a2382adaa967ea603122
WMacalester <willjpmaca@hotmail.com>
2023-11-12 15:36:29 +0000

    Cloud: Modify api-asg health check type

--- cloud/api.tf
+++ cloud/api.tf
@@ -208,7 +208,7 @@ resource "aws_autoscaling_group" "api_asg" {
     min_size                  = 0
     max_size                  = 1
     health_check_grace_period = 300
-    health_check_type         = "EC2"
+    health_check_type         = "ELB"
 }
 
 data "aws_iam_policy_document" "ecs_agent" {
@@ -226,12 +226,6 @@ resource "aws_iam_role" "ecs_agent" {
   name               = "ecs-agent"
   assume_role_policy = data.aws_iam_policy_document.ecs_agent.json
 }
-
-
-# resource "aws_iam_role_policy_attachment" "ecs_agent" {
-#   role       = aws_iam_role.ecs_agent.name
-#   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
-# }
 
 resource "aws_iam_instance_profile" "ecs_agent" {
   name = "ecs-agent"
